c466c5a6a5800c903330a00cac2b74c78bbaa2be
mayankrd <mayank.dyl@gmail.com>
2017-07-17 16:13:02 -0400

    updated page-edit

--- src/app/components/page/page-edit/page-edit.component.ts
+++ src/app/components/page/page-edit/page-edit.component.ts
@@ -12,10 +12,19 @@ export class PageEditComponent implements OnInit {
   userId: string;
   websiteId: string;
   pageId: string;
+  errFlag: boolean;
+  error: string;
+  alert: string;
+  page = {};
 
   constructor(private _pageService: PageService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+
+    // setting err flag as false by default
+    this.errFlag = false;
+    this.error = 'Enter the name of the Page';
+    this.alert = '* Enter the Page name';
 
     // fetching userId, websiteId and pageId from current route
     this.activatedRoute.params
@@ -26,6 +35,20 @@ export class PageEditComponent implements OnInit {
           this.pageId = params['pageId'];
         }
       );
+
+    // fetch current page details
+    this._pageService.findPageById(this.pageId)
+      .subscribe(
+        (data: any) => {this.page = data; console.log(this.page);}
+      );
+  }
+
+  updatePage(){
+
+  }
+
+  deletePage(){
+
   }
 
 }
