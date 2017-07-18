e750916dd5eb8c41be0df0f6ca8c4d7a274853f4
sesha sai srivatsav kuchibhotla <sesha@seshas-MacBook-Pro.local>
2017-07-18 10:10:51 -0400

    asd

--- server.js
+++ server.js
@@ -59,4 +59,6 @@ var serverSide = require('./server/app');
 serverSide(app);
 
 //Listen on provided port, on all network interfaces.
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+server.listen(process.env.PORT , () => console.log(`API running on localhost:${port}`));
+
+
