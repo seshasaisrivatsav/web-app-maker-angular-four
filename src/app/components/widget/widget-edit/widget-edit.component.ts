6628f66a402933cfb809765b9a4267bcb971e536
mayankrd <mayank.dyl@gmail.com>
2017-07-21 17:33:30 -0400

    added widget-edit

--- src/app/components/widget/widget-edit/widget-edit.component.ts
+++ src/app/components/widget/widget-edit/widget-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {WidgetService} from "../../../services/widget.service.client";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-widget-edit',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WidgetEditComponent implements OnInit {
 
-  constructor() { }
+  widgetId: string;
+  widget = {type: ''};
+  header = 'Header'; youtube = 'YouTube'; text = 'Text'; html = 'HTML'; image = 'Image';
+
+  constructor(private widgetService: WidgetService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+
+    // fetching widgetId from current url
+    this.activatedRoute.params
+      .subscribe(
+        (params) => {
+          this.widgetId = params['widgetId'];
+        }
+      );
+
+    // get widget type
+    this.widgetService.findWidgetById(this.widgetId)
+      .subscribe(
+        (data: any) => this.widget.type = data.type
+      );
   }
 
 }
