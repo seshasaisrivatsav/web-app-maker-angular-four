9d2eba231ee0c7376285849caa19987b8d9b3ce2
sesha sai srivatsav kuchibhotla <sesha@seshas-MacBook-Pro.local>
2017-07-20 21:57:05 -0400

    to heroku

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 // Point static path to dist -- For building
-// app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // CORS
 app.use(function(req, res, next) {
@@ -42,10 +42,10 @@ const api = require('./server/routes/api');
 app.use('/api', api);
 
 // For Build: Catch all other routes and return the index file
-// app.get('*', function (req, res) {
-//   const index = path.join(__dirname, 'dist', 'index.html');
-//   res.sendFile(index);
-// });
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'dist', 'index.html');
+  res.sendFile(index);
+});
 
 
 // Get port from environment and store in Express.
