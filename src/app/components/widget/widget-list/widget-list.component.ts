e67de8117b0cb00b0f67fd7555123ee9da64e5b2
mayankrd <mayank.dyl@gmail.com>
2017-07-20 20:41:21 -0400

    widget list updated

--- src/app/components/widget/widget-list/widget-list.component.ts
+++ src/app/components/widget/widget-list/widget-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgSwitch } from '@angular/common';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-widget-list',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WidgetListComponent implements OnInit {
 
-  constructor() { }
+  widget = { size: "1" };
+  userId: string;
+  websiteId: string;
+  pageId: string;
+
+  constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+
+    // fetch userId, pageId and websiteId from url
+    this.activatedRoute.params
+      .subscribe(
+        (params: any) => {
+          this.userId = params['userId'];
+          this.websiteId = params['websiteId'];
+          this.pageId = params['pageId'];
+        }
+      );
+
+  }
+
+  checkSafeHtml(html) {
+
+    // var html = this.widget.text;
+    // return $sce.trustAsHtml(html);
+  }
+
+  checkSafeYoutubeUrl(url) {
+
+    // var parts = url.split('/');
+    // var id = parts[parts.length - 1];
+    // url = "https://www.youtube.com/embed/" +id;
+    // console.log(url);
+    // return $sce.trustAsResourceUrl(url);
   }
 
 }
