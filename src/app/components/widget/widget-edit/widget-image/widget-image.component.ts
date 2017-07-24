c4457dbbae1ecc0fdcd0c51a85cc1c9acdda5cb8
mayankrd <mayank.dyl@gmail.com>
2017-07-24 18:17:52 -0400

    widget image updates

--- src/app/components/widget/widget-edit/widget-image/widget-image.component.ts
+++ src/app/components/widget/widget-edit/widget-image/widget-image.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {WidgetService} from '../../../../services/widget.service.client';
+import {environment} from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-widget-image',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WidgetImageComponent implements OnInit {
 
-  constructor() { }
+  widget = {};
+  userId: string;
+  websiteId: string;
+  pageId: string;
+  widgetId: string;
+  baseUrl: string;
+
+  constructor(private activatedRoute: ActivatedRoute, private widgetService: WidgetService) { }
 
   ngOnInit() {
+
+    // fetching baseUrl to server
+    this.baseUrl = environment.baseUrl;
+
+    // fetching all ids from route params
+    this.activatedRoute.params
+      .subscribe(
+        (params: any) => {
+          this.userId = params['userId'];
+          this.websiteId = params['websiteId'];
+          this.pageId = params['pageId'];
+          this.widgetId = params['widgetId'];
+        }
+      );
+
+    this.widgetService.findWidgetById(this.widgetId)
+      .subscribe(
+        (data: any) => this.widget = data,
+        (error: any) => console.log(error)
+      );
+
   }
 
 }
