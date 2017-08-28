bf9be106ad1ca7eec4ec0999727dc2e8f7e5276f
Sesha sai srivatsav Kuchibhotla <seshasaisrivatsav@gmail.com>
2017-08-28 10:53:48 -0400

    Changed baseUrl

--- src/app/services/widget.service.client.ts
+++ src/app/services/widget.service.client.ts
@@ -3,13 +3,14 @@
  */
 import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
+import {environment} from '../../environments/environment';
 import 'rxjs/Rx';
 
 @Injectable()
 
 export class WidgetService{
 
-  baseUrl = 'http://localhost:9000';
+  baseUrl = environment.baseUrl;
 
   constructor(private _http : Http){}
 
